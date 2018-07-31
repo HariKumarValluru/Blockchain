1a84e98b33a8210e35419f98155d5a98e9712c92
Hari Kumar Valluru <anil14353@gmail.com>
2018-07-31 22:20:43 +0530

    Using reduce and lambda functions

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,4 @@
+import functools
 # Initializing out empty blockchain List[]
 MINING_REWARD = 10
 
@@ -19,15 +20,12 @@ def get_balances(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx["amount"] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
+    # for tx in tx_sender:
+    #     if len(tx) > 0:
+    #         amount_sent += tx[0]
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_recive = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_recive += tx[0]
+    amount_recive = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
     return amount_recive - amount_sent
 
 def get_last_blockchain_value():
