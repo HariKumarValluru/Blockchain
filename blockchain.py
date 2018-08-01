db868c11aaeab47b7c539f6dd4a3c792f7861dcf
Hari kumar Valluru <anil14353@gmail.com>
2018-08-01 11:53:19 +0530

    importing only reduce

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,4 @@
-import functools
+from functools import reduce
 import hashlib
 import json
 # Initializing out empty blockchain List[]
@@ -22,12 +22,12 @@ def get_balances(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx["amount"] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+    amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
     # for tx in tx_sender:
     #     if len(tx) > 0:
     #         amount_sent += tx[0]
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_recive = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+    amount_recive = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
     return amount_recive - amount_sent
 
 def get_last_blockchain_value():
