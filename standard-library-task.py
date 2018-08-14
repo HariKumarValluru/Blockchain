dc242d21f06d362fe00a28f5a794093c5355cbc1
Hari kumar Valluru <anil14353@gmail.com>
2018-08-14 07:57:23 +0530

    removed files

--- standard-library-task.py
+++
@@ -1,8 +0,0 @@
-# 1) Import the random function and generate both a random number between 0 and 1 as well as a random number between 1 and 10.
-import random
-import datetime
-print(random.random())
-print(random.randint(1,10))
-
-# 2) Use the datetime library together with the random number to generate a random, unique value.
-print(str(datetime.datetime.now())+str(random.random()))
