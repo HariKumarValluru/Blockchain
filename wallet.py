cd974cac4755acb54ffadf9f8fba0d6d80aec12a
Hari Kumar Valluru <anil14353@gmail.com>
2018-08-14 17:50:36 +0530

    Generating public and private keys

--- wallet.py
+++ wallet.py
@@ -1,8 +1,14 @@
 from Cryptodome.PublicKey import RSA
 import Cryptodome.Random
+import binascii
 
 class Wallet:
     def __init__(self):
-        pass
+        private_key, public_key = self.generate_keys()
+        self.private_key = private_key
+        self.public_key = public_key
+        
     def generate_keys(self):
-        RSA.generate
+        private_key = RSA.generate(1024, Cryptodome.Random.new().read)
+        public_key = private_key.publickey()
+        return (binascii.hexlify(private_key.export_key(format='DER')).decode('ascii'), binascii.hexlify(public_key.export_key(format='DER')).decode('ascii'))
