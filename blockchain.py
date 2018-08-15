a11d95f8df8680bdc3360383602359269f3381dc
Hari kumar Valluru <anil14353@gmail.com>
2018-08-15 11:59:45 +0530

    Improving verification

--- blockchain.py
+++ blockchain.py
@@ -149,8 +149,6 @@ class Blockchain:
         if self.hosting_node == None:
             return False
         transaction = Transaction(sender, recipient, signature, amount)
-        if not Wallet.verify_transaction(transaction):
-            return False
         if Verification.verify_transaction(transaction, self.get_balances):
             self.__open_transactions.append(transaction)
             self.save_data()
@@ -177,12 +175,12 @@ class Blockchain:
         # Copy transaction instead of manipulating the original open_transactions list
         # This ensures that if for some reason the mining should fail, we don't have the reward transaction stored in the open transactions
         copied_transactions = self.__open_transactions[:]
+        for tx in copied_transactions:
+            if not Wallet.verify_transaction(tx):
+                return False
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block, 
 						copied_transactions, proof)
-        for tx in block.transactions:
-            if not Wallet.verify_transaction(tx):
-                return False
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
