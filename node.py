2e28e69e55b532e3018625976752459c32acee37
Hari kumar Valluru <anil14353@gmail.com>
2018-08-22 07:24:35 +0530

    changing default port to 5000

--- node.py
+++ node.py
@@ -190,7 +190,7 @@ def get_nodes():
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', type=int, default=5001)
+    parser.add_argument('-p', '--port', type=int, default=5000)
     args = parser.parse_args()
     port = args.port
     wallet = Wallet(port)
